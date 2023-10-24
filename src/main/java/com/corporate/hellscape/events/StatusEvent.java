c93d519e5d140d362645c473ca374484305ad900
ChaaZee <chazeden48@gmail.com>
2023-10-24 14:41:18 -0400

    status event update

--- src/main/java/com/corporate/hellscape/events/StatusEvent.java
+++ src/main/java/com/corporate/hellscape/events/StatusEvent.java
@@ -5,20 +5,14 @@ import com.corporate.hellscape.Hellscape;
 //NOTE: For issue #6, change this to abstract and create a concrete
 //      example child class that implements it for testing (see Hellscape.java)
 public abstract class StatusEvent extends Event {
-
-    //TODO: Remove, this is only here to make the skeleton compile
-    public StatusEvent() {} 
-
     //TODO: Implement isTriggered, mark it final, and have it re-dispatch to
     //      an abstract method of StatusEvent that takes *just the character*
     //      as its argument. It's okay if the testing class you make always
     //      returns true or false or something, since the character class
     //      doesn't have any fields yet
-    public boolean isTriggered(Hellscape hellscape) {
-        return true;
+    public final boolean isTriggered(Hellscape hellscape) {
+        return this.isTriggered(hellscape.getCharacter());
     }
 
-    //TODO: Remove this and let child classes that implement StatusEvent handle their own action
-    //      (e.g.: Your testing class)
-    //public void triggerAction(Hellscape hellscape) { }
+    public abstract boolean isTriggered(Character character);
 }
--- src/main/java/com/corporate/hellscape/events/StatusEventHungerLow.java
+++ src/main/java/com/corporate/hellscape/events/StatusEventHungerLow.java
@@ -5,9 +5,12 @@ import com.corporate.hellscape.Hellscape;
 public class StatusEventHungerLow extends StatusEvent{
 
     public void triggerAction(Hellscape hellscape) {
-        if(isTriggered(hellscape)){
-            hellscape.getCharacter().decreaseHealth();
-        }
+        hellscape.getCharacter().decreaseHealth();
     }
-    
+
+    public boolean isTriggered(Character character) {
+        if(character.getHunger() < 50)
+            return true;
+        return false;
+    }
 }
