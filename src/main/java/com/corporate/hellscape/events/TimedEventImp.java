a022d7a795538c0cbdbab025e1fb3b12a9f82c8b
MoAlsa7 <122761594+MoAlsa7@users.noreply.github.com>
2023-10-24 15:50:20 -0400

    7 time event (#22)
    * update
    
    * update
    
    * Update mo's intro.txt
    
    * update
    
    * update
    
    * updates
    
    * update #7 Timed event

---
+++ src/main/java/com/corporate/hellscape/events/TimedEventImp.java
@@ -0,0 +1,44 @@
+package com.corporate.hellscape.events;
+
+import java.time.LocalDateTime;
+
+import com.corporate.hellscape.Hellscape;
+import com.corporate.hellscape.character.Character;
+
+
+public class TimedEventImp extends TimedEvent {
+
+    private long delayDurationSeconds;
+    private LocalDateTime eventTime;
+
+
+    public TimedEventImp(int delayDurationSeconds,Hellscape hellscape) {
+        super(delayDurationSeconds, hellscape);
+        this.delayDurationSeconds = delayDurationSeconds;
+        eventTime = hellscape.getGameTime().plusSeconds(delayDurationSeconds);
+    }
+
+    public boolean isTriggered(Hellscape hellscape) {
+        LocalDateTime currentTime = hellscape.getGameTime();
+        return eventTime.isAfter(currentTime) ;
+    }
+
+   
+
+    @Override
+    protected void triggerAction(Hellscape hellscape) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'triggerAction'");
+    }
+
+   
+  
+
+  
+   
+    
+
+
+   
+    
+}
