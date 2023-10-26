0a8ba4db167a54e4ad6af10b94a746131cc54284
MoAlsa7 <122761594+MoAlsa7@users.noreply.github.com>
2023-10-26 15:39:52 -0400

    issue 13 hunger level

--- src/main/java/com/corporate/hellscape/events/TimedEvent.java
+++ src/main/java/com/corporate/hellscape/events/TimedEvent.java
@@ -3,16 +3,26 @@ package com.corporate.hellscape.events;
 import java.time.LocalDateTime;
 
 import com.corporate.hellscape.Hellscape;
+import com.corporate.hellscape.character.Character;
 
 public abstract class TimedEvent extends Event {
 
     private LocalDateTime eventTime;
+    private LocalDateTime starTime;
 
     protected TimedEvent(int delayDurationSeconds, Hellscape hellscape) {
         eventTime = hellscape.getGameTime().plusSeconds(delayDurationSeconds);
+        starTime = hellscape.getGameTime();
     }
 
     public boolean isTriggered(Hellscape hellscape) {
         return eventTime.isBefore(hellscape.getGameTime());
+    }
+
+    public void checkHunger(Hellscape hellscape, Character character) {
+        LocalDateTime currenTime = hellscape.getGameTime();
+        if (currenTime.getSecond()-starTime.getSecond()==30){
+            character.increaseHunger();
+        }
     }
 }
