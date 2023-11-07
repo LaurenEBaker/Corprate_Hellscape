87ecbea47048da31895db0ead45be8c601e3a01c
Chaz Eden <chazeden48@gmail.com>
2023-11-07 18:36:09 -0500

    Edited controller and game.fxml

--- src/main/java/com/corporate/hellscape/JavaFXController.java
+++ src/main/java/com/corporate/hellscape/JavaFXController.java
@@ -3,28 +3,64 @@ package com.corporate.hellscape;
 import java.io.IOException;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ProgressBar;
 import javafx.scene.image.Image;
 
 public class JavaFXController {
+    @FXML
     public Image image;
 
-    public ProgressBar HealthBar;
+    @FXML
+    public ProgressBar healthBar;
+    @FXML
+    public ProgressBar hungerBar;
+    public double hungerProgress = 1.0;
+    @FXML
+    public ProgressBar stressBar;
+    @FXML
+    public ProgressBar staminaBar;
+    @FXML
+    public ProgressBar hygieneBar;
+    @FXML
+    public ProgressBar funBar;
+    @FXML
+    public ProgressBar workLoadBar;
 
-    public ProgressBar HungerBar;
-    public double HungerProgress = 0.1;
+    @FXML
+    public Button deStressButton;
 
-    public ProgressBar StressBar;
-    public ProgressBar StaminaBar;
-    public ProgressBar HygieneBar;
-    public ProgressBar FunBar;
-    public ProgressBar WorkLoadBar;
+    public void workButtonClicked(ActionEvent event) throws IOException{
+        //Increase workLoadBar
+        //Play animation for working
+    }
 
-    public Button feedButton;
+    public void eatButtonClicked(ActionEvent event) throws IOException {
+        hungerBar.setProgress(hungerProgress);
+        hungerProgress -= 0.1;
+    }
 
-    public void feedButtonClicked(ActionEvent event) throws IOException {
-        HungerBar.setProgress(HungerProgress);
-        HungerProgress += 0.1;
+    public void sleepButtonClicked(ActionEvent event) throws IOException{
+        //Increase staminaBar
+        //Play animation for sleeping
+    }
+
+    public void enjoymentButtonClicked(ActionEvent event) throws IOException{
+        //Increase funBar
+        //Play animation for enjoyment
+    }
+
+    public void cleanButtonClicked(ActionEvent event) throws IOException{
+        //Increase hygieneBar
+        //Play animation for cleaning
+    }
+
+    public void saveGameButtonClicked(ActionEvent event) throws IOException{
+        //Save game
+    }
+
+    public void exitGameButtonClicked(ActionEvent event) throws IOException{
+        //Exit game
     }
 }
