3981f09f533f4b79e59915f56b0d970b38eb417e
Joseph Marlin <CW000005471@ups.com>
2023-11-14 15:41:10 -0500

    fix status low naming

--- src/main/java/com/corporate/hellscape/events/StatusEventHungerLow.java
+++ src/main/java/com/corporate/hellscape/events/StatusEventHungerLow.java
@@ -3,7 +3,7 @@ package com.corporate.hellscape.events;
 import com.corporate.hellscape.Hellscape;
 import com.corporate.hellscape.character.Character;
 
-public class StatusEventHungerHigh extends StatusEvent{
+public class StatusEventHungerLow extends StatusEvent{
 
     public void triggerAction(Hellscape hellscape) {
         hellscape.getCharacter().decreaseHealth();
