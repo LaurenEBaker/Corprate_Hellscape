7e26f67def15ce8a31c5a134d079174b95b50cf2
MoAlsa7 <122761594+MoAlsa7@users.noreply.github.com>
2023-11-18 15:59:40 -0500

    added shower and relax for issues 56 and 57

---
+++ src/main/java/com/corporate/hellscape/events/RelaxCharacterInputEvent.java
@@ -0,0 +1,30 @@
+package com.corporate.hellscape.events;
+
+import com.corporate.hellscape.Hellscape;
+import com.corporate.hellscape.character.Character;
+
+public class RelaxCharacterInputEvent extends Event {
+
+    public RelaxCharacterInputEvent(){ }
+
+    @Override
+    public boolean isTriggered(Hellscape hellscape) {
+      return true;
+    }
+
+    @Override
+    protected void triggerAction(Hellscape hellscape) {
+        Character character = hellscape.getCharacter();
+        int oldStress = character.getStress();
+
+        hellscape.getCharacter().decreaseStress();
+
+        int newStress = character.getStress();
+
+        System.out.println(String.format(
+            "Character relaxed. stress went from %d to %d",
+            oldStress,
+            newStress ));
+    }
+    
+}
