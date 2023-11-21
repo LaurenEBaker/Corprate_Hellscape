e08121d7d4e03803034f6e0c627d71e642f69717
makairasoft <jmarlin@makairasoftware.com>
2023-11-21 14:19:17 -0500

    #60 register input events on button presses

--- src/main/java/com/corporate/GuiControls/GameLoop.java
+++ src/main/java/com/corporate/GuiControls/GameLoop.java
@@ -7,23 +7,26 @@ import javafx.animation.AnimationTimer;
 
 public class GameLoop extends AnimationTimer {
 
-    private static long SIMULATION_CYCLE_TIME = 1000;
+    private static long SIMULATION_CYCLE_TIME = 100000000;
 
-    private Hellscape _hellscape = new Hellscape();
+    private Hellscape _hellscape;
     private long _lastTimestamp = 0;
     private JavaFXController _parentController;
 
-    public GameLoop(JavaFXController parentController) {
+    public GameLoop(JavaFXController parentController, Hellscape hellscape) {
         super();
 
+        _hellscape = hellscape;
         _parentController = parentController;
     }
 
     public void handle(long now) {
 
         //Only run a simulation pass if enough real-world time has passed
-        if(!(_lastTimestamp == 0 || _lastTimestamp + SIMULATION_CYCLE_TIME <= now))
+        if(!(_lastTimestamp == 0 || _lastTimestamp + SIMULATION_CYCLE_TIME <= now)) 
             return;
+
+        _lastTimestamp = now;
 
         //TODO: Check for game not running no more
         _hellscape.SimulateOnce();
