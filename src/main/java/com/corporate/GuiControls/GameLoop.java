2fe02552b4bf8567fa571725cc61cb11189fa2c4
Joshua Anderson <JoshScottA47@gmail.com>
2023-11-24 18:45:36 -0500

    Place the progress bar code into a function.

--- src/main/java/com/corporate/GuiControls/GameLoop.java
+++ src/main/java/com/corporate/GuiControls/GameLoop.java
@@ -21,26 +21,9 @@ public class GameLoop extends AnimationTimer {
         _parentController = parentController;
     }
 
-    public void handle(long now) {
+    public void updateProgressBar(Character slave){
 
-        //Only run a simulation pass if enough real-world time has passed
-        if(!(_lastTimestamp == 0 || _lastTimestamp + SIMULATION_CYCLE_TIME <= now)) 
-            return;
-
-        _lastTimestamp = now;
-
-        Character slave = _hellscape.getCharacter();
-
-        
-    
-
-        //TODO: Check for game not running no more
-        _hellscape.SimulateOnce();
-
-        for(String message : _hellscape.getPendingMessages())
-            _parentController.showLogMessage(message);
-
-        //TODO: Link up stat progress bar here
+        //Link up stat progress bar here to character stat by dividing with (double)100.
         _parentController.healthBar.setProgress(slave.getHealth()/100.0);
         _parentController.hungerBar.setProgress(slave.getHunger()/100.0);
         _parentController.stressBar.setProgress(slave.getStress()/100.0);
@@ -49,8 +32,25 @@ public class GameLoop extends AnimationTimer {
         _parentController.funBar.setProgress(slave.getFun()/100.0);
         _parentController.workLoadBar.setProgress(slave.getWorkload()/100.0);
 
-       //Debug Section
-    //    _parentController.showLogMessage(String.valueOf(slave.getHealth()/100.0));
+    }
+
+    public void handle(long now) {
+
+        //Only run a simulation pass if enough real-world time has passed
+        if(!(_lastTimestamp == 0 || _lastTimestamp + SIMULATION_CYCLE_TIME <= now)) 
+            return;
+
+        _lastTimestamp = now;
+
+        //TODO: Check for game not running no more
+        _hellscape.SimulateOnce();
+
+        for(String message : _hellscape.getPendingMessages())
+            _parentController.showLogMessage(message);
+
+        updateProgressBar(_hellscape.getCharacter());
+
+        
 
         
        
